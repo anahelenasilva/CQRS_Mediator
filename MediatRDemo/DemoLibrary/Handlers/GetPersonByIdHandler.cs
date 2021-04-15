2527bfa9a651bc325cfe780a816baa5f92d5c226
Ana Helena <anahelenarp@hotmail.com>
2021-04-15 08:16:51 -0300

    Adding api to test MediatR (get by id and insert)

---
+++ MediatRDemo/DemoLibrary/Handlers/GetPersonByIdHandler.cs
@@ -0,0 +1,31 @@
+﻿using DemoLibrary.DataAccess;
+using DemoLibrary.Models;
+using DemoLibrary.Queries;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.Handlers
+{
+    public class GetPersonByIdHandler : IRequestHandler<GetPersonByIdQuery, PersonModel>
+    {
+        private readonly IMediator _mediator;
+
+        public GetPersonByIdHandler(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        //showing the concept of a MediatR item, a handler, calling another MediatR
+        public async Task<PersonModel> Handle(GetPersonByIdQuery request, CancellationToken cancellationToken)
+        {
+            var results = await _mediator.Send(new GetPersonListQuery());
+            var output = results.FirstOrDefault(f => f.Id == request.Id);
+            return output;
+        }
+    }
+}
